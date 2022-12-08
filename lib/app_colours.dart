647be938998691ef98ad0aab4c932195e0feb132
Ala2Adel <58287956+Ala2Adel@users.noreply.github.com>
2022-12-09 01:31:52 +0200

    first commit

---
+++ lib/app_colours.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xFF000000);
+  static const Color primaryCyan = Color(0xFF0000CE);
+  static const Color orange = Color(0xFFF84711);
+  static const Color lightOrange = Color(0xFFFF8E42);
+  static const Color lightPink = Color(0xFFFF87B3);
+  static const Color yellow = Color(0xFFFFE621);
+  static const Color greyText = Color(0xFF032326);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color lightGrey = Color(0xFFF5F5F9);
+  static const Color darkGrey = Color(0xFF808080);
+
+
+}
